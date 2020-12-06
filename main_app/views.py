c66262356d656bc42515aefd49ac26adc0eecd6f
Arun Xena <1runx3na2@gmail.com>
2020-12-07 00:33:29 +0530

    Final commit monday-xena

--- main_app/views.py
+++ main_app/views.py
@@ -32,20 +32,24 @@ def models(request, model_name):
 
 def search(request, range_name):
     args =range_name
+    args2 =range_name
+    args3 =range_name
     if request.method == 'POST':
         oem = request.POST.get('search_l')
         oem1 = oem.lower()
         oem2 = oem.capitalize()
+        oem3 = oem.capitalize()
         oem1 = oem1.encode('ascii', 'ignore').decode()
         oem1 = oem1.title()
-    args = oem1
-    args2 = oem2
-    print(args2)
+        args = oem1
+        args2 = oem2
+        args3 = oem3
     batteryrange = Batteryrange.objects.filter(range_name=range_name)
     batterymodel = Batterymodel.objects.filter(range_id__range_name=range_name)
-    compatability = Compatability.objects.all().filter(Q(OEM__icontains=args) | Q(model_id__part_number__icontains=args2)).distinct()
-    print(compatability)
-    context = {'batteryrange': batteryrange, 'batterymodel': batterymodel, 'compatability': compatability,'args': args, 'range_name': range_name, 'args2': args2 }
+    compatability = Compatability.objects.all().filter(OEM__contains=args)
+    bb = Batterymodel.objects.filter(part_number__contains=args2)
+    ll = Compatability.objects.filter(vehicle_models__contains=args3)
+    context = {'batteryrange': batteryrange, 'batterymodel': batterymodel, 'compatability': compatability,'args': args, 'range_name': range_name, 'args2': args2, 'bb': bb, 'll': ll }
     return render(request, 'main_app/search.html', context)
 
 
