7c78fd7d987c2439b2a62f78fcff44920a20071a
Arun Xena <1runx3na2@gmail.com>
2020-12-06 19:16:40 +0530

    Xena-new

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,12 @@ from . import views
 urlpatterns = [
     path('', views.main_app, name="main_app"),
     path('shop', views.showcase, name="showcase"),
-    path('details', views.details, name="details"),
+    path('details/<str:range_name>+range', views.details, name="details"),
+    path('details/search/<str:range_name>+range', views.search, name="search"),
+    path('details/search/models/<str:model_name>+range', views.models, name="models"),
+    path('details/search/models/search/<str:range_name>+range', views.search, name="models"),
     path('contact', views.contacts, name="contact"),
+    path('models', views.models, name="models"),
+    path('shop2', views.amaron, name="amaron"),
+    path('about', views.about, name="about"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,15 +3,51 @@ from .models import *
 
 # Create your views here.
 def main_app(request):
-    return render(request, 'main_app/index.html')
+    batteryrange = Batteryrange.objects.all()
+    context = {'batteryrange': batteryrange}
+    return render(request, 'main_app/index.html', context)
 
 def showcase(request):
-    batterymodels = Batterymodel.objects.all()
-    context = {'batterymodels': batterymodels}
+    batteryrange = Batteryrange.objects.all()
+    context = {'batteryrange': batteryrange}
     return render(request, 'main_app/products.html', context)
 
-def details(request):
-    return render(request, 'main_app/details.html')
+def details(request, range_name):
+    related = Batteryrange.objects.all()
+    batteryrange = Batteryrange.objects.filter(range_name=range_name)
+    batterymodel = Batterymodel.objects.filter(range_id__range_name=range_name)
+    applications = Application.objects.filter(range_id__range_name=range_name)
+    context = {'batteryrange': batteryrange, 'batterymodel': batterymodel, 'related': related, 'applications': applications}
+    return render(request, 'main_app/details.html', context)
+
+
+def models(request, model_name):
+    batterymodel = Batterymodel.objects.filter(part_number=model_name)
+    compatability = Compatability.objects.all()
+    print(compatability)
+    print(model_name)
+    context = {'batterymodel': batterymodel, 'compatability': compatability, 'model_name': model_name}
+    return render(request, 'main_app/models.html', context)
+
+def search(request, range_name):
+    args =range_name
+    if request.method == 'POST':
+        oem = request.POST.get('search_l')
+        args = oem
+    print(args)
+    batteryrange = Batteryrange.objects.filter(range_name=range_name)
+    batterymodel = Batterymodel.objects.filter(range_id__range_name=range_name)
+    compatability = Compatability.objects.all().filter(OEM=args)
+    context = {'batteryrange': batteryrange, 'batterymodel': batterymodel, 'compatability': compatability,'args': args, 'range_name': range_name }
+    return render(request, 'main_app/search.html', context)
+
+
+def amaron(request):
+    return render(request, 'main_app/shop2.html')
+
+
+def about(request):
+    return render(request, 'main_app/about.html')
 
 def contacts(request):
     return render(request, 'main_app/contact.html')
